03ea74ce9fca1c7e4abcfb26846ee90c0211cdce
nando <muhammad.rezki.ananda@gmail.com>
2021-11-18 09:42:56 +0700

    first commit | https://youtu.be/RJ7f6XqmU-w

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller/counter_controller.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final counterC = Get.put(CounterController());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
+    return Obx(() => MaterialApp(
+          home: HomePage(),
+          theme: counterC.isDark.value ? ThemeData.dark() : ThemeData.light(),
+        ));
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final c = Get.find<CounterController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Obx(
+          () => Text(
+            'Angka ${c.counter}',
+            style: TextStyle(fontSize: 35),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          c.increment();
+          c.changeTheme();
+        },
+      ),
     );
   }
 }
