ca02714dad0929edc0a48526c5247451919e3152
nando <muhammad.rezki.ananda@gmail.com>
2021-11-18 10:04:10 +0700

    first commit | https://youtu.be/Fqc2iLc5R88

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/models/orang.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,12 +18,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  var orang = Orang(nama: "Rezki", umur: 21).obs;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Obx(() => Text(
+              "Nama saya ${orang.value.nama}",
+              style: TextStyle(fontSize: 35),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          orang.update((_) {
+            orang.value.nama = orang.value.nama.toString().toUpperCase();
+          });
+        },
+      ),
     );
   }
 }
---
+++ lib/models/orang.dart
@@ -0,0 +1,11 @@
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+
+class Orang {
+  // var nama = "Rezki".obs;
+  // var umur = 25.obs;
+
+  var nama;
+  var umur;
+
+  Orang({this.nama, this.umur});
+}
